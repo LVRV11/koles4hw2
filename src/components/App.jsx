6423318860388f29360ba89fb2d3aedcbfe89bb7
Dmitrii Lavrov <lavrovdmitrii@gmail.com>
2023-05-05 12:01:35 +0200

    4ds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
     </div>
   );
 };
+
+export default App;
